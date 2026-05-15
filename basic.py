921a81bb57743750016f4894aa74c3efa9784040
Avivvi-b <binattamit@gmail.com>
2026-05-15 19:46:52 +0300

    add Python OOP concepts

--- basic.py
+++ basic.py
@@ -1,12 +1,20 @@
-class Numbers:
-    lst = []
+class Animal:
+    is_breathing = True
 
-    def __init__(self, lst):
-        self.lst=lst
-    
-    def __len__(self):
-        return len(self.lst)
-    
-d1 = Numbers([1,2,3])
+    def __init__(self, name, age):
+        self.name=name
+        self.age=age
 
-print(len(d1))
+    def __str__(self):
+        return f"The animal's name is {self.name} and it is {self.age} years young"
+    
+    def animal_Sound(self, sound):
+        return f"weeee"
+    
+class Dog(Animal):
+    def animal_Sound(self, sound):
+        return f"iiiii"
+d1=Dog("Mathew",5)
+print(d1.animal_Sound("Helolo"))
+a1=Animal("Rabbit",3)
+print(a1.animal_Sound("Elolo"))
